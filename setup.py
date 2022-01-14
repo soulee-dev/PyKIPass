672d8797bae85eff1c5d0bea7216463b8d24456e
DESKTOP-BM8MHJJ\a0103 <a010393223@gmail.com>
2022-01-15 00:27:17 +0900

    Fix setuptools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='PyKIPass',
-    version='1.0.0',
+    version='1.1.0',
     license='MIT',
     author='alus20x',
     author_email='alus20x@gmail.com',
@@ -10,5 +10,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     long_description=open('README.md', encoding='UTF8').read(),
     url='https://github.com/alus20x/PyKIPass',
-    packages = setuptools.find_packages(),
+    packages=setuptools.find_packages()
 )
